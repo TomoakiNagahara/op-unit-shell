9718f3572d1dbc645269db6e5eba7f466dea63b3
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-03-29 18:06:51 +0900

    New: Shell.class.php | (empty)

---
+++ ci/Shell.php
@@ -0,0 +1,31 @@
+<?php
+/**	op-unit-shell:/ci/Shell.php
+ *
+ * @created    2026-03-30
+ * @license    Apache-2.0
+ * @package    op-unit-shell
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+$ci = OP()->Unit()->CI()->Config();
+
+//	Include sub directory files.
+$name = basename(__FILE__);
+$name = explode('.', $name)[0];
+foreach( glob(__DIR__."/{$name}/*.php") as $path ){
+	require_once($path);
+}
+
+//	...
+return $ci->Get();
