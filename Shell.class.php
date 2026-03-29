9c16fc977055e4d0ab33b7b1644a0808be85dc6e
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-03-29 18:58:29 +0900

    Add: Shell > implements > IF_SHELL

---
+++ Shell.class.php
@@ -0,0 +1,37 @@
+<?php
+/**	op-unit-shell:/Shell.class.php
+ *
+ * @created    2026-03-29
+ * @license    Apache-2.0
+ * @package    op-unit-shell
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP\UNIT;
+
+/**	Use
+ *
+ */
+use OP\IF_SHELL;
+use OP\OP_CORE;
+use OP\OP_CI;
+
+/**	Shell
+ *
+ * @created    2026-03-29
+ */
+class Shell implements IF_SHELL
+{
+	/**	Trait.
+	 *
+	 */
+	use OP_CORE, OP_CI;
+}
