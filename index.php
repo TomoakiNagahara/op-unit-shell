7e61c2907763000f6c13e01c60ffc425ff4469a0
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-03-29 18:52:44 +0900

    Add: index.php | include > Shell.class.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**	op-unit-shell:/index.php
+ *
+ * @created    2026-03-29
+ * @license    Apache-2.0
+ * @package    op-unit-shell
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/**	Include
+ *
+ */
+require_once(__DIR__.'/Shell.class.php');
